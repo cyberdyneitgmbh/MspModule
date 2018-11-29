536e0fe3e1c994ac50592edcb8e1498ae8f4e752
JanDurryCyberdyne <40829022+JanDurryCyberdyne@users.noreply.github.com>
2018-11-29 17:32:26 +0100

    Testing Msp-GetHelp

--- Public/Get-MspEventLog.ps1
+++ Public/Get-MspEventLog.ps1
@@ -6,5 +6,15 @@
 #> 
 
 function Get-MspEventLog {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [String]$Name
+    )
+    if ($Name -like "Msp-GetHelp") {
+        write-host "Help"
+    } else {
+        write-host "no Help"
+    }
     write-host "Get-MspEventLog wurde aufgerufen"
 }
