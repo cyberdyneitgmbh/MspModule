4f562a7aebf7d06fe035a46e290bd367b59fbb73
Durry <durry@cyberdyne.de>
2018-12-04 09:51:33 +0100

    Get-MspHelp überarbeitet

--- Public/Get-MspHelp.ps1
+++ Public/Get-MspHelp.ps1
@@ -8,62 +8,65 @@
 function Get-MspHelp {
     [CmdletBinding()]
     param (
-        [parameter(Mandatory = $true, Position=0)]
-        [string]$FunctionName,
-
-        [parameter(Mandatory = $true, Position=1)]
-        [Object]$Parameters
+        [parameter(Mandatory = $false, Position = 0)]
+        [ValidateSet("About_Msp_EventLog")]
+        [string]$Command
     )
-    $Type = $FunctionName.Substring($FunctionName.IndexOf("-")+1)
-    $Verb = $FunctionName.SubString(0,$FunctionName.IndexOf("-"))
+    if (!($PSBoundParameters.ContainsKey('Command'))) {
+        $ShortDescription = "Zeigt Hilfe zum Windows-PowerShell-Modul MspModule an."
+        $LongDescription = "Wenn Sie wissen moechten, wie man am besten mit dem MspModule arbeitet,`n
+finden sie hier die passenden Lösungen. Durch angabe von bestimmten Parametern können Sie so die `n
+alles rund um die verschiedenen Methoden erfahren."
+        $Examples = "Nutzen Sie die die Funktion Get-Help mit dem Parameter <-Command-> [string], um mehr über einen`n
+bestimmten Befehl zu erfahren:`n`n
+Parameter für Command können sein:`n
+About_Msp_Eventlog`n
+`n`n
+        
+Durch Eingabe von 'Get-Help About_Msp_Eventlog' finden sie alles rund um das Thema EventLogs mit RMM!"
 
-    Switch ($Verb) {
+        
+        return "<#
 
-        'Get' { 
-            $Synopsis = 'Über diese Methode kann ein Objekt des Typs {0} über das "MspModule" abgerufen werden. Alle benötigten Parameter werden markiert als benötigte Parameter um dich beim Programmieren zu unterstützen.' -F $Type
-            $Description = 'Das Objekt {0} stellt verschiedene Parameter bereit, allerdings sind nicht alle nötig um ein neues Objekt zu erzeugen. Für mehr Informationen nutze Get-{0} | Format-List, um eine Liste der möglichen Parameter zu erhalten' -F $Type
-            $Example = "Beispiel: {0}-{1} " -f $Verb, $Type
-            $First = $Parameters | Select-Object -first 1
-            foreach ($Parameter in $Parameters) {
-                $Example += "<-$Parameter [Wert]> "
-            }
-            $Example += " Erhält ein neues {0}. Es wurden alle (möglichen) Parameter angegeben. Der Parameter {1} muss angegeben werden." -f $Type, $First
+.THEMA:
+GET-MSPHELP
 
-        }
-        'New' { 
-            $Synopsis = 'Diese Funktion erstellt eine neue {0} über das "MspModule". Alle benötigten Parameter werden markiert als benötigte Parameter um dich beim Programmieren zu unterstützen.' -F $Type
-            $Description = 'Das Objekt {0} stellt verschiedene Parameter bereit, allerdings sind nicht alle nötig um ein neues Objekt zu erzeugen. Für mehr Informationen nutze Get-{0} | Format-List, um eine Liste der möglichen Parameter zu erhalten' -F $Type
-            $Example = "Beispiel: {0}-{1} " -f $Verb, $Type
-            $First = $Parameters | Select-Object -first 1
-            foreach ($Parameter in $Parameters) {
-                $Example += "<-$Parameter [Wert]> "
-            }
-            $Example += " erzeugt ein neues {0}. Es wurden alle Parameter angegeben. Der Parameter {1} muss angegeben werden." -f $Type, $First
-        }
-        'Set' { 
-            $First = $Parameters | Select-Object -first 1
-            $Synopsis = 'Diese Funktion setzt einen oder mehrere Werte eines {0} über das "MspModule". Alle angebenen Parameter werden bei einem bestimmten Objekt ersetzt. Der [Wert] {1} wird benötigt um das Objekt zu identifizieren. Alle benögtigen Parameter werden makiert, um dich beim Programmieren zu unterstützen.' -F $Type, $First
-            $Description = 'Das Objekt {0} stellt verschiedene Parameter bereit, jeder dieser Parameter kann ersetzt werden. Bitte achte darauf, dass der [Wert] für {1} einmalig sein muss.' -F $Type, $First
-            $Example = "Beispiel: {0}-{1} " -f $Verb, $Type
-            foreach ($Parameter in $Parameters) {
-                $Example += "<-$Parameter [Wert]> "
-            }
-            $Example += " setzt die Parameter für das eindeutige Objekt {0}. Es wurden alle Parameter angegeben. Mit dem Parameter {1} wird das Object identifiziert." -f $Type, $First
+.KURZBESCHREIBUNG
+$ShortDescription
+
+.DETAILBESCHREIBUNG
+$LongDescription
+
+.BEISPIELE
+$Examples
+
+.LINK
+https://github.com/cyberdyneitgmbh/MspModule
+
+#>"
+
+    }
+    Switch ($Command) {
+
+        'About_Msp_EventLog' { 
+            $ShortDescription = "Noch in Entwicklung."
+            $LongDescription = "Noch in Entwicklung"
+            $Examples = "Noch in Entwicklung"
         }
     }
     return "<#
 
-.SYNOPSIS
-$Synopsis
+.THEMA:
+About_Msp_EventLog
 
-.DESCRIPTION
-$Description
+.KURZBESCHREIBUNG
+$ShortDescription
 
-.EXAMPLE
-$Example
+.DETAILBESCHREIBUNG
+$LongDescription
 
-.NOTES
-.Dieses Skript befindet sich noch in der Entwicklung
+.BEISPIELE
+$Examples
 
 .LINK
 https://github.com/cyberdyneitgmbh/MspModule
@@ -71,5 +74,3 @@ https://github.com/cyberdyneitgmbh/MspModule
 #>"
 
 }
-
-Get-MspHelp New-Test @('Name', 'Alter', 'Geschlecht')
