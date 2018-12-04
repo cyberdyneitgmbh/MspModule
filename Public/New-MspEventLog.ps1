8a0bc821ee3460578d54b41d03460fde573a5010
Durry <durry@cyberdyne.de>
2018-12-04 14:41:19 +0100

    Erweiterung von MspEvent -> New-MspEventLog Erstellt ein neues Log -> Write-MspEventLog schreibt einen Eintrag in ein Msp Log -> Standard ist MspEvent

--- Public/New-MspEventLog.ps1
+++ Public/New-MspEventLog.ps1
@@ -6,5 +6,55 @@
 #> 
 
 function New-MspEventLog {
-    write-host "New-MspEventLog wurde aufgerufen"
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $True, Position = 0)]
+        [string]$Source,
+
+        [parameter(Mandatory = $false)]
+        [string]$ComputerName
+    )
+
+    begin {
+        $LastErrorActionPreference = $ErrorActionPreference
+        $ErrorActionPreference = "stop"
+        $Logname = "MspEvent"
+    }
+
+    process {
+        if ([System.Diagnostics.EventLog]::Exists($Logname)) {
+            Write-Verbose "+ $Logname vorhanden"
+            Write-Verbose "+ Prüfe, ob Quelle $Source in $Logname vorhanden"
+            if (!([System.Diagnostics.EventLog]::SourceExists($Source))) {
+                try {
+                    Write-Verbose "+ Erstelle neue Quelle $Source in $Logname"
+                    New-EventLog -LogName $Logname -Source $Source
+                    Write-Verbose "+ Eventlog $Logname wurde mit der Quelle $Source erstellt"
+                }
+                catch {
+                    Write-Error "- Es ist ein Fehler bei der Erstellung des MspEventLogs aufgetreten"
+                }
+            }
+            else {
+                Write-Verbose "+ Quelle $Source in $Logname vorhanden"
+            }
+        }
+        else {
+            Write-Verbose "+ $Logname existiert nicht und wird mit der Quelle $Source neu erstellt."
+            try {
+                Write-Verbose "+ Erstelle neue Quelle $Source in $Logname"
+                New-EventLog -LogName $Logname -Source $Source
+                Write-Verbose "+ Eventlog $Logname wurde mit der Quelle $Source erstellt"
+            }
+            catch {
+                Write-Error "- Es ist ein Fehler bei der Erstellung des MspEventLogs aufgetreten"
+            }
+        }
+    }
+        
+    end {
+        $ErrorActionPreference = $LastErrorActionPreference
+        Write-Verbose "+ Funktion New-MspEventLog beendet"
+    }
+    
 }
