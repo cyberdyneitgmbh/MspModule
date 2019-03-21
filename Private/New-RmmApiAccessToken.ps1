9a48d687df56d66aa6b7b9717ac5a55596393fa3
JanDurryCyberdyne <40829022+JanDurryCyberdyne@users.noreply.github.com>
2019-03-21 11:18:50 +0100

    Adding RMM-Integration

---
+++ Private/New-RmmApiAccessToken.ps1
@@ -0,0 +1,61 @@
+﻿<# 
+
+.DESCRIPTION 
+    Accesstoken for RMM-Api connection (pinotage)
+
+#>
+
+function New-AemApiAccessToken {
+    [CmdletBinding()]
+    param (
+        [string]$apiUrl,
+        [string]$apiKey,
+        [string]$apiSecretKey
+    )   
+    begin {
+        # Erroraction preference
+        $LastErrorActionPreference = $ErrorActionPreference
+        $ErrorActionPreference = 'stop'
+                
+        # Errorflag
+        $Errorstate = $false 
+
+        # Verbose parameter
+        if ($PSBoundParameters.ContainsKey('Verbose')) {
+            $VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference')
+        }
+        
+        # Specify security protocols
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+
+        # Convert password to secure string
+        $securePassword = ConvertTo-SecureString -String 'public' -AsPlainText -Force
+
+        # Define parameters for Invoke-WebRequest cmdlet
+        $params = @{
+            Credential  =	New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ('public-client', $securePassword)
+            Uri         =	'{0}/auth/oauth/token' -f $apiUrl
+            Method      =	'POST'
+            ContentType = 'application/x-www-form-urlencoded'
+            Body        = 'grant_type=password&username={0}&password={1}' -f $apiKey, $apiSecretKey
+        }
+    }            
+    process {
+        Write-Verbose "Empfange $AccessToken"
+        try {
+            $AccessToken = (Invoke-WebRequest @params -UseBasicParsing | ConvertFrom-Json).access_token
+        }
+        catch {
+            write-Verbose "Fehler waehrend des WebRequests."
+            Write-Error $_.Exception.Message
+            $Errorstate = $true
+        }
+    }            
+    end {
+        $ErrorActionPreference = $LastErrorActionPreference
+        if ($Errorstate) {
+            return $null
+        }
+        return $AccessToken
+    }
+}
---
+++ Private/New-RmmApiRequest.ps1
@@ -0,0 +1,65 @@
+﻿<# 
+
+.DESCRIPTION 
+    Accesstoken for RMM-Api connection (pinotage)
+
+#>
+function New-AemApiRequest {
+    param 
+    (
+        [string]$apiUrl,
+        [string]$apiAccessToken,
+        [string]$apiMethod,
+        [string]$apiRequest,
+        [string]$apiRequestBody
+    )
+    begin {
+        Write-Verbose "Variablen werden prepariert"
+        # Erroraction preference
+        $LastErrorActionPreference = $ErrorActionPreference
+        $ErrorActionPreference = 'stop'
+            
+        # Errorflag
+        $Errorstate = $false
+
+        # Verbose parameter
+        if ($PSBoundParameters.ContainsKey('Verbose')) {
+            $VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference')
+        }
+
+        # Define parameters for Invoke-WebRequest cmdlet
+        $params = @{
+            Uri         =	'{0}/api{1}' -f $apiUrl, $apiRequest
+            Method      =	$apiMethod
+            ContentType	= 'application/json'
+            Headers     =	@{
+                'Authorization'	=	'Bearer {0}' -f $apiAccessToken
+            }
+        }
+
+    }            
+    process {
+        # Add body to parameters if present
+        If ($apiRequestBody) {
+            $params.Add('Body', $apiRequestBody)
+        }
+
+        # Make request
+        try {
+            Write-Verbose "Getting Request..."
+            $Request = (Invoke-WebRequest @params).Content
+        }
+        catch {
+            Write-Verbose "Fehler waehrend der Anfrage."
+            Write-Error $_.Exception.Message
+            $Errorstate = $true
+        }
+    }            
+    end {
+        $ErrorActionPreference = $LastErrorActionPreference
+        if ($Errorstate) {
+            return $null
+        }
+        return $Request
+    }
+}
